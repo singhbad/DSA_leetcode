15fe3510040a07d0314d6b2077ee4a8d36349c4c
Badal Singh <badal.singh2432@gmail.com>
2026-06-25 18:32:57 +0000

    added

--- DoublyLinkedList/Traverse.py
+++ DoublyLinkedList/Traverse.py
@@ -11,5 +11,34 @@ class DoublyLinkedList:
         self.tail = None
         self.size = 0
 
+    def traverse_forward(self):
+        curr = self.head
 
+        while curr:
+            print(curr.data, end=" <-> ")
+            curr = curr.next
+
+        print("None")
+
+    def append(self, data):
+
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+        self.size += 1
+
+dll = DoublyLinkedList()
+dll.append(10)
+dll.append(20)
+dll.append(30)
+dll.append(40)
     
+dll.traverse_forward()
