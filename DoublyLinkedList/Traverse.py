5d12e6ec440654f38d98809abf49567cf7b38db4
Badal Singh <badal.singh2432@gmail.com>
2026-06-25 17:49:04 +0000

    DoublyLinkedList

---
+++ DoublyLinkedList/Traverse.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+
+    
