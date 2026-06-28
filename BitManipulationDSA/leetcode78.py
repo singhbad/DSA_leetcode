335b3198a83eb06fe25b672bcdfd1a5c7a6045d0
Badal Singh <badal.singh2432@gmail.com>
2026-06-28 07:45:05 +0000

    add

---
+++ BitManipulationDSA/leetcode78.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+
+        total_subsets = 1 << n
+        result = []
+
+        for num in range (0, total_subsets):
+            lst = []
+            for i in range(n):
+                if num & (1 << i) != 0:
+                    lst.append(nums[i])
+
+            result.append(lst)
+
+        return result
+        
